c42b78c5480dceffa1235103ee6a1b2ebf1c671e
Robin Toy <robin@strobe-it.co.uk>
2015-07-08 21:39:49 +0100

    Graph clean up
    The code used to generate the graphs has been cleaned up and
    non-required code removed.

--- check-list/scp/checklist-statistics.php
+++ check-list/scp/checklist-statistics.php
@@ -48,8 +48,6 @@ include (CHECKLIST_JPGRAPH.'jpgraph_line.php');
 include (CHECKLIST_JPGRAPH.'jpgraph_bar.php');
 	
 # initialisation: 
-#error_reporting(E_ALL);
-/*import_request_variables("gpc");*/
 extract($_REQUEST);
 $num_columns=6;
 
@@ -89,14 +87,11 @@ $current_user = $thisstaff->getFirstName() . ' ' . $thisstaff->getLastName();
 		# first row contains calendar
 		$pn = array('&laquo;'=>'./checklist-statistics.php?vorige', '&raquo;'=>'./checklist-statistics.php?volgende'); 
 		print "<tr><td valign='top' nowrap>\n";
-		//$time = time(); 
-		//$today = date('j',$time); 
 		$days = array($today=>array(NULL,NULL, 
 			'<span style="color: red; font-weight: bold; font-size: larger; 
 			text-decoration: blink;">' .$today.'</span>')); 
 		# last 2 on next line is # chars for columnname
 		show_calendar("checklist-statistics.php",$year,$month);
-		#print "</td>\n\n<!-- end off calendar -->\n\n";
 		print "\n\n<!-- end off calendar -->\n\n";
 		print "( <a href='./checklist-statistics.php'><b>".$lang[42]."</b></a> ) ";
 		print "<!-- start log of date -->\n";
@@ -118,38 +113,29 @@ $current_user = $thisstaff->getFirstName() . ' ' . $thisstaff->getLastName();
 		# function to calculate and displat the daily percentile scores
 		# get number of checkpoints 
 		$totaal=numberOfCheckpoints();
+		
 		# how many have daily been entered?
 		$query = "SELECT day(datum) AS nr, count(distinct(ref))  AS ingevuld FROM ". CHECKLIST_TABLE_ENTRIES ." 
 			WHERE month(datum)=".$month." AND year(datum)=".$year." GROUP BY day(datum) 
 			ORDER BY datum" ;
 		#echo "<!-- ".$query." -->\n";
+		
 		# initialise array
 		for ($i=1;$i<=31; $i++) {
 			$l2datay[$i]=0;
 		}
+		
 		# get results
 		$result = db_query($query);
 		while ($row = db_fetch_array($result, MYSQL_ASSOC)) {
-			#print $row["dag"]." = array('./index.php?datum='".$row["dag"].",'linked-day'";
 			$l2datay[$row["nr"]-1]=round(($row["ingevuld"]/$totaal)*100) ;
-			#print $row["nr"]." - ".$row["ingevuld"]." - ".$l2datay[$row["nr"]]."<br>";
 		}
 	
-
 		// Create the graph.
 		$graph = new Graph(600,200,"auto");
 		$graph->SetScale("textlin");
 		$graph->SetMargin(40,130,20,40);
 		$graph->SetShadow();
-
-		// Create the linear error plot
-		#$l1plot=new LinePlot($l1datay);
-		#$l1plot->SetColor("red");
-		#$l1plot->SetWeight(2);
-		#$l1plot->SetLegend("Entered % per day");
-
-		//Center the line plot in the center of the bars
-		#$l1plot->SetBarCenter();
 
 		// Create the bar plot
 		$bplot = new BarPlot($l2datay);
@@ -158,7 +144,6 @@ $current_user = $thisstaff->getFirstName() . ' ' . $thisstaff->getLastName();
 
 		// Add the plots to t'he graph
 		$graph->Add($bplot);
-		#$graph->Add($l1plot);
 
 		$graph->title->Set($lang[7]);
 
@@ -182,64 +167,47 @@ $current_user = $thisstaff->getFirstName() . ' ' . $thisstaff->getLastName();
 			WHERE month(datum)=".$month." AND year(datum)=".$year." GROUP BY door 
 			ORDER BY door" ;
 		#echo "<!-- ".$query." -->\n";
+		
 		#get results
 		$result = db_query($query);
 		$i=0;
 		$totaal=0;
 		while ($row = db_fetch_array($result, MYSQL_ASSOC)) {
-			#print $row["dag"]." = array('./index.php?datum='".$row["dag"].",'linked-day'";
 			$databarx[$i]=$row["door"];
 			$databary[$i]=$row["ingevuld"];
 			$totaal+=$row["ingevuld"];
-			#print $row["door"]."-".$row["ingevuld"]."-".$databarx[$i]."-".$databary[$i]."<br>";
 			$i++;
 		}
 		$aantal=$i-1;
 		for ($teller=0;$teller<=$aantal;$teller++) {
 			$databary[$teller]=round(($databary[$teller]/$totaal)*100,2);
 		}
-		# joke.... Just uncomment and enter here the name of your boss :-)
-		#array_push($databarx,"kostermm");
-		#array_push($databary,-15.3);
 		array_multisort ($databarx, $databary); 
-	
-		#$datax=$gDateLocale->GetShortMonth();
 
 		// Create the graph.
 		$graph = new Graph(600,200,"auto");
 		// Use a "text" X-scale
 		$graph->SetScale("textlin");
-		#$graph->SetMargin(40,130,20,40);
 		$graph->SetShadow();
-		#$graph->xaxis->SetTickLabels($datax);
-
-		//Center the line plot in the center of the bars
-		#$l1plot->SetBarCenter();
 
 		// Specify X-labels
 		$graph->xaxis->SetTickLabels($databarx);
-		#$graph->xaxis->SetTextLabelInterval(1);
-		#$graph->xaxis->SetTextTickInterval(3);
+		
 		// Create the bar plot
 		$bplot = new BarPlot($databary);
 		$bplot->SetFillColor("orange");
 		$bplot->SetLegend($lang[6]);
 		$bplot->SetWidth(0.4);
 		$bplot->value-> Show();
-		#$bplot->value->SetAngle(45); 
 
 		// Add the plots to t'he graph
 		$graph->Add($bplot);
-		#$graph->Add($l1plot);
 
 		$graph->title->Set(sprintf($lang[10],$year,$month));
 
 		$graph->xaxis->title->Set($lang[11]);
-		#$graph->xaxis->SetTextLabelIntervall(4 );
-		#$graph->xaxis->SetLabelMargin(50);
 		$graph->yaxis->title->Set($lang[12]);
 		$graph->title->SetFont(FF_FONT1,FS_BOLD);
-		#$graph->title->SetFont(FF_ARIAL,FS_BOLD);
 		$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
 		$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
 
@@ -255,17 +223,14 @@ $current_user = $thisstaff->getFirstName() . ' ' . $thisstaff->getLastName();
 	
 		# get number of checkpoints op
 		$totaal=numberOfCheckpoints();
+		
 		# how may have been entered daily?
 		$query = "SELECT day(datum) AS dag, month(datum) AS maand, count(distinct(ref)) AS ingevuld 
 			FROM ". CHECKLIST_TABLE_ENTRIES .",". CHECKLIST_TABLE_CHECKLIST ." WHERE DATE_SUB(CURDATE(),INTERVAL 12 month)<=datum AND ref=". CHECKLIST_TABLE_CHECKLIST .".id and disabled=0 
 			GROUP BY day(datum),month(datum) 
 			ORDER BY year(datum),month(datum),day(datum)";
-
 		#echo "<!-- ".$query." -->\n";
-		# initialise array
-		#for ($i=1;$i<=31; $i++) {
-		#	$l2datay[$i]=0;
-		#}
+		
 		# get results
 		$result = db_query($query);
 		$teller=0;
@@ -277,8 +242,6 @@ $current_user = $thisstaff->getFirstName() . ' ' . $thisstaff->getLastName();
 				$databary[$teller]=$databary[$teller]/$aantaldagen;
 				$databary[$teller]=$databary[$teller]/$totaal*100;
 				$aantaldagen=1;
-				# houskeeping work
-				#print "$teller , $maand, $databary[$teller]<br>";
 				$teller++;
 				$databary[$teller]=0;
 			}
@@ -288,36 +251,18 @@ $current_user = $thisstaff->getFirstName() . ' ' . $thisstaff->getLastName();
 			$maand=$row["maand"];
 			$databarx[$teller]=$maand;
 			$databary[$teller]+=$row["ingevuld"];
-			#$databary[$teller]+=$row["ingevuld"];
-			
-			#print $row["dag"]." = array('./index.php?datum='".$row["dag"].",'linked-day'";
-			#$l2datay[$row["nr"]-1]=round(($row["ingevuld"]/$totaal)*100) ;
-			#print $row["nr"]." - ".$row["ingevuld"]." - ".$l2datay[$row["nr"]]."<br>";
 		}
 		$databary[$teller]=$databary[$teller]/$aantaldagen;
 		$databary[$teller]=$databary[$teller]/$totaal*100;
-		#print "$teller , $maand, $databary[$teller]<br>";
 	
-		#$datax=$gDateLocale->GetShortMonth();
-
 		// Create the graph.
 		$graph = new Graph(600,200,"auto");
 		$graph->SetScale("textlin");
-		#$graph->SetMargin(40,130,20,40);
 		$graph->SetShadow();
-		#$graph->xaxis->SetTickLabels($datax);
-
-		// Create the linear error plot
-		#$l1plot=new LinePlot($l1datay);
-		#$l1plot->SetColor("red");
-		#$l1plot->SetWeight(2);
-		#$l1plot->SetLegend("Ingevuld % per dag");
-
-		//Center the line plot in the center of the bars
-		#$l1plot->SetBarCenter();
 
 		// Specify X-labels
 		$graph->xaxis->SetTickLabels($databarx);
+		
 		// Create the bar plot
 		$bplot = new BarPlot($databary);
 		$bplot->SetFillColor("orange");
@@ -326,7 +271,6 @@ $current_user = $thisstaff->getFirstName() . ' ' . $thisstaff->getLastName();
 
 		// Add the plots to t'he graph
 		$graph->Add($bplot);
-		#$graph->Add($l1plot);
 
 		$graph->title->Set($lang[13]);
 
